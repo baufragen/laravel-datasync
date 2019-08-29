014ec7d57990e77ac277eb076e873d23639248a5
Frank Tiersch <mail@ftiersch.de>
2019-08-29 09:13:54 +0200

    Make automatic syncing disablable

--- src/Interfaces/DataSyncing.php
+++ src/Interfaces/DataSyncing.php
@@ -2,8 +2,10 @@
 
 namespace Baufragen\DataSync\Interfaces;
 
+use Baufragen\DataSync\Helpers\DataSyncCollector;
+
 interface DataSyncing {
     public function getSyncName();
     public function automaticDataSyncEnabled();
-    public function beforeDataSync();
+    public function beforeDataSync(DataSyncCollector $collector);
 }
