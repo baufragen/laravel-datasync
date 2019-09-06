9bc3000cfc86d9d3f6b0b636220f33f78f98196c
Frank Tiersch <mail@ftiersch.de>
2019-09-06 16:34:48 +0200

    Started dashboard

--- routes/routes.php
+++ routes/routes.php
@@ -1,12 +1,12 @@
 <?php
 
-Route::prefix('data_sync')->group(function() {
+Route::prefix('datasync')->group(function() {
 
     Route::post('handle_incoming', [\Baufragen\DataSync\Controllers\DataSyncController::class, 'handleIncomingSync'])->name('dataSync.handle');
 
     Route::prefix('dashboard')->group(function () {
 
-        Route::get('/', [\Baufragen\DataSync\Controllers\DataSyncController::class, 'view'])->name('dataSync.dashboard.view');
+        Route::get('/', [\Baufragen\DataSync\Controllers\DashboardController::class, 'view'])->name('dataSync.dashboard.view');
 
     });
 
