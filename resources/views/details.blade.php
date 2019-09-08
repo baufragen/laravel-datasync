1995b3bb925dd49c5ceeae2dfe422acd4fe4997d
Frank Tiersch <mail@ftiersch.de>
2019-09-08 19:34:35 +0200

    Dashboard

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,6 +1,16 @@
 @extends('dataSync::layouts.default')
 
 @section('content')
-    <h1>Log <small>{{ $log->created_at->format('d.m.Y H:i') }}</small></h1>
+    <div class="jumbotron">
+        <h1 class="display-4">Log</h1>
+        <p class="lead">{{ $log->created_at->format('d.m.Y H:i') }} <small>on</small> {{ $log->connection }}</p>
+    </div>
 
+    <div id="jsonData">
+    </div>
+
+    <script>
+        var wrapper = document.getElementById("jsonData");
+        jsonTree.create({{ $log->payload }}, wrapper)
+    </script>
 @endsection
