50fcbc32aecd01fb50ccfff63eae8020273d3136
Frank Tiersch <mail@ftiersch.de>
2022-08-06 10:43:01 +0100

    Add WithoutDataSync Middleware

--- src/Helpers/DataSyncHandler.php
+++ src/Helpers/DataSyncHandler.php
@@ -52,8 +52,6 @@ class DataSyncHandler {
                 }
                 HandleDataSync::dispatch($collector);
             });
-
-        $this->dataCollectors = collect();
     }
 
     protected function createDataCollectorForModel(DataSyncing $model, string $collectorClass) {
